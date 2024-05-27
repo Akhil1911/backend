a531b0ae95edb0f649d0615ca00641a89645300f
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-05-27 12:15:59 +0530

    Utils code discussion

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Changes");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./db/connectDB.js";
+const app = express();
+
+//config dotenv
+dotenv.config();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//connecting database
+connectDB();
