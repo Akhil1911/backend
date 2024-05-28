8acbcdb28bbccb65e8c6afebe5c412bdc6fc7985
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-05-28 09:52:12 +0530

    Add cloudinary

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,13 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-//connecting database
-connectDB();
+//connecting database and listening
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
