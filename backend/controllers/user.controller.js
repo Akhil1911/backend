9b0d4396ca70d2afde67707d8aa50624d70fa4a7
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-05-29 16:12:19 +0530

    user register controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,41 @@
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const userRegister = async (req, res) => {
   try {
-    res.status(200).send("OK");
+    const { username, email, password, fullName } = req.body;
+
+    //checking all fields are filled or not
+    if (
+      [username, email, password, fullName].some(
+        (val) => val?.trim() === "" || val === undefined
+      )
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All Fields Are Required",
+      });
+    }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(409).json({
+        success: false,
+        message: "Already Registered, Please Login",
+      });
+    }
+
+    const avatarPath = req.files?.avatar && req.files?.avatar[0]?.path;
+    const coverImagePath =
+      (req.files?.coverImage && req.files?.coverImage[0]?.path) || "";
+    if (!avatarPath) {
+      return res.status(400).json({
+        success: false,
+        message: "Avatar is Required",
+      });
+    }
+
+    const avatar = await uploadOnCloudinary(avatarPath);
+    const coverImage = await uploadOnCloudinary(coverImagePath);
+    return res.json({ avatar, coverImage });
   } catch (error) {
     res.status(error.code || 500).json({
       success: false,
--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,10 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 import fs from "fs";
-
+dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_secret: process.env.CLOUDINARY_PI_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
@@ -12,14 +13,15 @@ const uploadOnCloudinary = async (localFilePath) => {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
-      folder: "uploads",
-      use_filename: true,
-      unique_filename: true,
       overwrite: false,
+      unique_filename: true,
+      use_filename: true,
+      folder: "uploads",
     });
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
+    console.log(error);
     fs.unlinkSync(localFilePath);
     return null;
   }
